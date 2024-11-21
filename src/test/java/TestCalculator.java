6c674a133246271e1390f53fd5f6a4f7801db6d9
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-11-21 20:04:58 +0700

    test commit

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -3,4 +3,15 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestCalculator {
+    @Test
+    public void testSum() {
+        Calculator calc = new Calculator();
+        Assert.assertEquals(calc.sum(3, 4), 7);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calc = new Calculator();
+        Assert.assertEquals(calc.sub(1,1),0);
+    }
 }
