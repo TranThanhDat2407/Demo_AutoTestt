57b1a4cb54d257610798ad9d0649e1eab512a2be
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-11-21 20:11:25 +0700

    dev pass

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,11 +3,11 @@ package org.example;
 public class Calculator implements ICalculator{
     @Override
     public Integer sum(Integer a, Integer b) {
-        return a+ b;
+        return a+b;
     }
 
     @Override
     public Integer sub(Integer a, Integer b) {
-        return a+b;
+        return a-b;
     }
 }
